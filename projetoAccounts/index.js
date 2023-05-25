1a82af27601fcf2f897e680a3324f171115b2920
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-05-24 23:31:49 -0300

    ajuste de import/export

--- projetoAccounts/index.js
+++ projetoAccounts/index.js
@@ -1,4 +1,13 @@
-//modulos externos
+
 import  Inquirer  from "inquirer";
 import chalk from "chalk";
 import { readFileSync } from 'fs';
+
+function operation(){
+    inquirer.prompt([{
+        type: 'list',
+        name: 'action',
+        message: 'O que você deseja fazer?',
+        choices: ['Criar conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
+    }]).then().catch(err => console.log(err))
+}
