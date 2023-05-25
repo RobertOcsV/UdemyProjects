7f2e8f9e4a86441a6b3e39ebc8abb102e3780e87
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-05-24 23:40:42 -0300

    action capture

--- projetoAccounts/index.js
+++ projetoAccounts/index.js
@@ -1,7 +1,11 @@
 
-import  Inquirer  from "inquirer";
+import  inquirer  from "inquirer";
 import chalk from "chalk";
 import { readFileSync } from 'fs';
+
+
+operation();
+
 
 function operation(){
     inquirer.prompt([{
@@ -9,5 +13,11 @@ function operation(){
         name: 'action',
         message: 'O que você deseja fazer?',
         choices: ['Criar conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
-    }]).then().catch(err => console.log(err))
+    }]).then((answer)=>{
+        const action = answer['action']
+
+        console.log(action)
+
+    })  
+    .catch(err => console.log(err))
 }
