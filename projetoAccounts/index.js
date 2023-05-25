263e44041c9bdfd449f27cb487b562f777e229d2
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-05-25 16:55:32 -0300

    criando account

--- projetoAccounts/index.js
+++ projetoAccounts/index.js
@@ -12,12 +12,23 @@ function operation(){
         type: 'list',
         name: 'action',
         message: 'O que você deseja fazer?',
-        choices: ['Criar conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
+        choices: ['Criar Conta', 'Consultar Saldo', 'Depositar', 'Sacar', 'Sair']
     }]).then((answer)=>{
         const action = answer['action']
+
+        if(action === 'Criar Conta'){
+            createAccount();
+        }
 
         console.log(action)
 
     })  
     .catch(err => console.log(err))
+}
+
+//create an account
+
+function createAccount(){
+    console.log(chalk.bgGreen.black('Parabéns por escolher o nosso banco!'));
+    console.log(chalk.green('Defina as opções da sua conta a seguir...'))
 }
