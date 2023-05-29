9c18b90a301de35e1ad7ca3c8e7fdf6027fc36cb
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-05-29 14:20:54 -0300

    criação e instanciação de contas + fixbug

--- projetoAccounts/index.js
+++ projetoAccounts/index.js
@@ -42,10 +42,25 @@ function buildAccount(){
         const accountName = answer['accountName']
         console.info(accountName);
 
-        if(!fs.mkdirSync('accounts')) {
+        if(!fs.existsSync('accounts')) {
             fs.mkdirSync('accounts');
         }
-    }).catch(err => console.log(err))
+        if(fs.existsSync(`accounts/${accountName}.json`)){
+            console.log(chalk.bgRed.black('Essa conta já existe, escolha outra nome!'));
+            buildAccount();
+            return;
+        }
+
+        fs.writeFileSync(`accounts/${accountName}.json`, '{"balance": 0}',
+        function (err){
+            console.log(err);
+        }, 
+        )
+
+        console.log(chalk.green('Parabéns, a sua conta foi criada!'))
+        operation();
+    })
+    .catch(err => console.log(err))
 
 
 }
