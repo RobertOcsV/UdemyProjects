5a221fdf83b23fbaf12449e19e15dc5d162aaa89
Robert <61297366+RobertOcsV@users.noreply.github.com>
2023-06-04 02:37:39 -0300

    definindo a porta, escutando porta 3000

--- express/index.js
+++ express/index.js
@@ -1,1 +1,11 @@
 import express from "express";
+const app = express()
+const port = 3000 // variavel de ambiente
+
+app.get('/', (req, res) => {
+    res.send('Olá Mundo!!')
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
