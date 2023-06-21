5969e49ee2472ae40bc334461ec2fe812db0ecfe
RobertOcsV <robertoliveiracor@hotmail.com>
2023-06-21 18:46:27 -0300

    atrelar o que veio na url para o que vem do banco

--- express/index.js
+++ express/index.js
@@ -9,11 +9,17 @@ const basePath = path.join(__dirname, 'templates')
 
 
 
+app.get('/users/:id', (req, res) => {
+    const id = req.params.id
+
+    //leitura da tabela users, resgatar um usuário do banco
+    console.log(`Estamos buscando pelo usuário: ${id}`)
+
+    res.sendFile(`${basePath}/users.html`)
+})
+
 app.get('/', (req, res) => {
     res.sendFile(`${basePath}/index.html`)
-
-
-
 })
 
 app.listen(port, () => {
