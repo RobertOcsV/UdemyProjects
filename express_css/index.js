04baa3624087dd12b7f90361f0b3cb2e7581d84a
RobertOcsV <robertoliveiracor@hotmail.com>
2023-06-25 07:20:03 -0300

    como estilizar com express

---
+++ express_css/index.js
@@ -0,0 +1,34 @@
+const express = require('express')
+const path = require('path')
+const app = express()
+const port = 3000 // variavel de ambiente
+
+const users = require('./users')
+
+//Ler o body
+
+app.use(
+    express.urlencoded({
+        extended: true,
+    }),
+    )
+    
+app.use(express.json())
+
+// arquivos estaticos
+app.use(express.static('public'))
+
+const basePath = path.join(__dirname, 'templates')
+
+app.use('/users', users)
+
+app.get('/', (req, res) => {
+    res.sendFile(`${basePath}/index.html`)
+})
+
+app.listen(port, () => {
+    console.log(`App rodando na porta ${port}`)
+})
+
+
+
