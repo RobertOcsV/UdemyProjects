f700b27a255f3675d505166a1b9a5d811812b67f
RobertOcsV <robertoliveiracor@hotmail.com>
2023-07-05 03:36:07 -0300

    arquivos estáticos css

---
+++ CursoCompletoNodeJS/template_engine/9_css/index.js
@@ -0,0 +1,80 @@
+const express = require("express")
+const exphbs = require("express-handlebars")
+
+const app = express()
+
+const hbs = exphbs.create({
+    partialsDir: ['views/partials']
+})
+
+app.engine('handlebars', exphbs.engine()) 
+app.set('view engine', 'handlebars')
+
+app.use(express.static('public'))
+
+app.get('/dashboard', (req, res) => {
+
+        const pessoas = [
+            {idade: 12, nome: "Lucas"},
+            {idade: 17, nome: "Jorge"},
+            {idade: 19, nome: "Rogerio"}
+        ]
+   
+    res.render('dashboard',  {pessoas} )
+})
+
+app.get('/post', (req, res) => {
+    const post = {
+        title: 'Aprender Node.js',
+        category: 'Javascript',
+        body: 'Este artigo vai te ajudar a aprender nodejs..',
+        comments: 4
+    }
+
+    res.render('blogpost', { post })
+} )
+
+app.get('/blog', (req, res) => {
+    const posts = [
+        {
+            title: "Aprender Node.js",
+            category: "Javascript",
+            body: "php",
+            comments: 5
+        },
+        {
+            title: "Aprender Ruby",
+            category: "Javascript",
+            body: "teste",
+            comments: 5
+        },
+        {
+            title: "Aprender Mince",
+            category: "Javascript",
+            body: "loop",
+            comments: 5
+        },
+    ]
+
+    res.render("blog", { posts })
+} )
+
+app.get('/', (req, res) => {
+    
+    const user = {
+        name: "Matheus",
+        surname: "Loló",
+        age: 20
+    }
+
+    const auth = false
+
+    const approved = false
+
+    res.render('home', {user: user, auth, approved})
+})
+
+app.listen(3000, () => {
+    console.log('App Funcionando!')
+})
+
