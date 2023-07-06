b3d8966506fc7782879136626f67b346d93c195c
RobertOcsV <robertoliveiracor@hotmail.com>
2023-07-06 08:42:13 -0300

    projeto exemplo prática finalizado

--- CursoCompletoNodeJS/template_engine/projeto_final_teste/index.js
+++ CursoCompletoNodeJS/template_engine/projeto_final_teste/index.js
@@ -6,19 +6,48 @@ const app = express()
 const hbs = exphbs.create({
     partialsDir: ['views/partials']
 })
-
 app.engine('handlebars', exphbs.engine()) 
 app.set('view engine', 'handlebars')
 
 app.use(express.static('public'))
 
+const produtos = [
+
+    {  
+       id: 1,
+       name: "Pão francês" ,
+       price: "R$ 0,80"           
+    },
+    {  
+        id: 2,
+        name: "Bolo de morango" ,
+        price: "R$ 19,90"
+    },
+    {  
+        id: 3,
+        name: "Coxinha" ,
+        price: "R$ 7,80"
+    },
+    {  
+        id: 4,
+        name: "Café puro" ,
+        price: "R$ 4"
+    },
+
+]
 
 
 
+
+app.get('/produto/:id', (req, res) => {
+    const produto = produtos[parseInt(req.params.id) -1]
+
+    res.render('produto', { produto })
+})
 
 
 app.get('/', (req, res) => {
-    res.render('home')
+    res.render('home', { produtos } )
 })
 
 app.listen(3000, () => {
