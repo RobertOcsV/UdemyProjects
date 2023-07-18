d20c4f26291774711998e393e84ea2cca88a1b8b
RobertOcsV <robertoliveiracor@hotmail.com>
2023-07-18 15:25:57 -0300

    resgatando dados (SELECT)

--- CursoCompletoNodeJS/3_NODE_E_MYSQL/3_resgatar_dados/index.js
+++ CursoCompletoNodeJS/3_NODE_E_MYSQL/3_resgatar_dados/index.js
@@ -30,12 +30,30 @@ app.post('/books/insertbook', (req, res) => {
     conn.query(sql, (err)=> {
         if(err){
             console.log(err)
+            return
         }
 
-        res.redirect('/')
+        res.redirect('/books')
 
     })
 })
+
+app.get('/books', (req, res) => {
+    const sql = 'SELECT * FROM books'
+
+    conn.query(sql, function(err, data) {
+        if(err){
+            console.log(err)
+            return
+        }
+        const books = data
+
+        console.log(books)
+
+        res.render('books', { books })
+    })
+})
+
 
 
 const conn = mysql.createConnection({
